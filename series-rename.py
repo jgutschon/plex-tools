1d542a748bff980256b49df21f7af026337eaffa
Jon Gutschon <jongutschon123@gmail.com>
2022-07-09 22:36:41 -0400

    improvements to series renaming

--- series-rename.py
+++ series-rename.py
@@ -1,41 +1,62 @@
 import os
 import re
 
+# File types
+junk_files = [
+    'txt',
+    'png',
+    'jpg',
+    'jpeg',
+    'nfo',
+    'info',
+    'exe',
+]
+
+sub_files = [
+    'srt',
+    'smi',
+    'ssa',
+    'ass',
+    'vtt',
+]
+
 path = os.getcwd()
 series_name = path.split('/')[-1]
 
 season_regex = '(?:(?<=(Season ))\d{1,2}|(?<=([s|S]))\d{2})'
 episode_regex = '(?:(?<=(Episode ))\d{1,2}|(?<=([e|E]))\d{2})'
 
-for root, seasons, files in os.walk(os.getcwd()):
-    for season in seasons:
-        # Find season number
-        result = re.search(season_regex, season)
-        season_num = '0'
-        if result:
-            season_num = result.group()
-            if len(season_num) == 1:
-                season_num = '0' + season_num
-        
-            # Rename season
-            os.rename(season, f"Season {season_num}")
+for root, dirs, files in os.walk(path):
+    for file in files:
+        ext = file.split('.')[-1]
 
-        for root, seasons, episodes in os.walk(f"{os.getcwd()}/Season {season_num}"):
-            for episode in episodes:
-                # Find episode number
-                result = re.search(episode_regex, episode)
-                episode_num = '0'
-                if result:
-                    episode_num = result.group()
-                    if len(episode_num) == 1:
-                        episode_num = '0' + episode_num
+        # Clean junk files
+        if ext in junk_files:
+            os.remove(file)
+        else:
+            # Find season number
+            result = re.search(season_regex, file)
+            season_num = '0'
+            if result:
+                season_num = result.group()
+                if len(season_num) == 1:
+                    season_num = '0' + season_num
 
-                # Find episode title and extension
-                title = episode.split(' - ')[-1].split('.')[0].split('(')[0].strip(' ')
-                ext = episode.split('.')[-1]
+                # Plex formatted season name
+                season_name = f"Season {season_num}"
+                if not os.path.isdir(f"{path}/{season_name}"):
+                    os.mkdir(f"{path}/{season_name}")
 
-                # Rename episode
-                os.rename(
-                    f"Season {season_num}/{episode}",
-                    f"Season {season_num}/{series_name} - s{season_num}e{episode_num} - {title}.{ext}"
-                )
+            # Find episode number
+            result = re.search(episode_regex, file)
+            episode_num = '0'
+            if result:
+                episode_num = result.group()
+                if len(episode_num) == 1:
+                    episode_num = '0' + episode_num
+
+            # Rename episode
+            os.rename(
+                f"{os.path.join(root, file)}",
+                f"{season_name}/{series_name} - s{season_num}e{episode_num}.{ext}"
+            )
